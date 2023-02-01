816363e9a7adcf29921980dcb8a1ca724c449dd5
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2023-02-01 12:13:01 +0100

    selectors

--- src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
+++ src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
@@ -1,35 +1,16 @@
 package com.lasalle.automation.vueling.web;
 
-import org.junit.Assert;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
-import java.nio.file.StandardCopyOption;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 /**
  * - Window: get, getTitle, getCurrentUrl, getPageSource, close, quit
@@ -56,62 +37,32 @@ public class WebDriverOptionsTest {
 
         System.setProperty ("webdriver.chrome.driver","/home/s2o/tmp/chromedriver_linux64/chromedriver" );
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         LOGGER.debug("driver started");
 
+        // Navigation
         driver.get("https://the-internet.herokuapp.com");
         driver.getTitle();
         driver.getCurrentUrl();
         driver.getPageSource();
-
         driver.navigate().to("https://the-internet.herokuapp.com/abtest");
         driver.navigate().back();
         driver.navigate().forward();
         driver.navigate().refresh();
+        Assertions.assertThat(driver.getCurrentUrl()).isEqualTo("https://the-internet.herokuapp.com/abtest");
+        LOGGER.debug("navigation ok");
 
+        // Selectors
         driver.navigate().to("https://the-internet.herokuapp.com");
+        driver.findElement(By.id("page-footer"));
         driver.findElement(By.linkText("JavaScript Alerts")).click();
-        List<WebElement> buttons = driver.findElements(By.cssSelector("button"));
-        buttons.get(0).click();
+        driver.findElement(By.cssSelector("#content > div > ul > li:nth-child(1) > button")).click();
         driver.switchTo().alert().accept();
-        buttons.get(1).click();
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/ul/li[2]/button")).click();
         driver.switchTo().alert().dismiss();
-
-        // Esperas - Explícitas FluentWait
-        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-        driver.findElement(By.cssSelector("#checkbox-example > button")).click();
-        Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver)
-                .withTimeout(Duration.of(30, ChronoUnit.SECONDS))
-                .pollingEvery(Duration.of(2, ChronoUnit.SECONDS))
-                .ignoring(NoSuchElementException.class);
-        WebElement fluentElement = fluentWait.until(new Function<WebDriver, WebElement>() {
-            @Override
-            public WebElement apply(WebDriver webDriver) {
-                return webDriver.findElement(By.id("message"));
-            }
-        });
-        String fluentElementText = fluentElement.getText();
-        Assert.assertTrue(fluentElement.isDisplayed());
-        LOGGER.debug("finish element, fluentElementText:[{}]", fluentElementText);
-
-        // Esperas - Implícitas implicitlyWait
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-        driver.findElement(By.cssSelector("#checkbox-example > button")).click();
-        WebElement implicitWaitElement = driver.findElement(By.id("message"));
-        String implicitWaitElementText = implicitWaitElement.getText();
-        Assert.assertTrue(implicitWaitElement.isDisplayed());
-        LOGGER.debug("finish element, implicitWaitElementText:[{}]", implicitWaitElementText);
-
-        // Esperas - Explícitas WebDriverWait
-        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
-        driver.findElement(By.cssSelector("#start > button")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
-        String text = element.getText();
-        Assert.assertTrue(element.isDisplayed());
-        LOGGER.debug("finish element, WebDriverWait, text:[{}]", text);
+        List<WebElement> buttons = driver.findElements(By.cssSelector("button"));
+        Assertions.assertThat(buttons.size()).isEqualTo(3);
+        LOGGER.debug("selectors ok");
 
         driver.close();
         LOGGER.debug("driver closed");
