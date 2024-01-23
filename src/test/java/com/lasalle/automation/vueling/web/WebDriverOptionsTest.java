bcb4c35bba0e8f8ce07458fe7977dbd46274ad47
sergi <serg@es.es>
2024-01-23 22:41:11 +0100

    first test

--- src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
+++ src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
@@ -1,11 +1,15 @@
 package com.lasalle.automation.vueling.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.lang.invoke.MethodHandles;
 
 /**
@@ -26,20 +30,27 @@ public class WebDriverOptionsTest {
 
     private static WebDriver driver;
 
+    @BeforeEach
+    public void setUp() {
+        LOGGER.debug("start testWebDrive");
+        // TODO download from https://www.selenium.dev/ecosystem/
+        File currentDirFile = new File(".webDriver/chromedriver.exe");
+        System.setProperty ("webdriver.chrome.driver",currentDirFile.getAbsolutePath() );
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.close();
+        LOGGER.debug("driver closed");
+    }
+
     @Test
     public void testWebDrives() throws InterruptedException
     {
-        LOGGER.debug("start testWebDrive");
-
-        // TODO download from https://www.selenium.dev/ecosystem/
-        System.setProperty ("webdriver.chrome.driver","C:\\...\\chromedriver.exe" );
-
-        driver = new ChromeDriver();
         driver.manage().window().maximize();
         LOGGER.debug("driver starter");
-
-        driver.close();
-        LOGGER.debug("driver closed");
+        Assertions.assertNotNull(driver);
     }
 
 }
