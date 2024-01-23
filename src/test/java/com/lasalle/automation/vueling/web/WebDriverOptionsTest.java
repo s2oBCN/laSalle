fd8e2258994ab5c795dbe6c98ee3081252d649e3
sergi <serg@es.es>
2024-01-23 23:30:37 +0100

    TODO

--- src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
+++ src/test/java/com/lasalle/automation/vueling/web/WebDriverOptionsTest.java
@@ -1,6 +1,8 @@
 package com.lasalle.automation.vueling.web;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,6 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.lang.invoke.MethodHandles;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -32,25 +35,28 @@ import java.time.temporal.ChronoUnit;
  */
 public class WebDriverOptionsTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        LOGGER.debug("start testWebDrive");
+        // TODO download from https://www.selenium.dev/ecosystem/
+        File currentDirFile = new File(".webDriver/chromedriver.exe");
+        System.setProperty ("webdriver.chrome.driver",currentDirFile.getAbsolutePath() );
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        LOGGER.debug("driver started");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        LOGGER.debug("driver closed");
+    }
+
     @Test
     public void testWebDrives() {
         LOGGER.debug("start testWebDrive");
-
-        // TODO download from https://www.selenium.dev/ecosystem/
-        System.setProperty ("webdriver.chrome.driver","C:\\...\\chromedriver.exe" );
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        LOGGER.debug("driver started");
-
-        // Esperas - Implícitas implicitlyWait => no such element: Unable to locate element
-        // Se aplica a TODOS los elementos
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-        driver.findElement(By.cssSelector("#checkbox-example > button")).click();
-        WebElement implicitWaitElement = driver.findElement(By.id("message"));
-        String implicitWaitElementText = implicitWaitElement.getText();
-        Assertions.assertThat(implicitWaitElement.isDisplayed()).isTrue();
-        LOGGER.debug("finish element, implicitWaitElementText:[{}]", implicitWaitElementText);
 
         // Esperas - Explícitas WebDriverWait => el elemento existe...
         // pero no es visible o no cumple alguna condición
@@ -61,21 +67,6 @@ public class WebDriverOptionsTest {
         String text = element.getText();
         Assertions.assertThat(element.isDisplayed()).isTrue();
         LOGGER.debug("finish element, WebDriverWait, text:[{}]", text);
-
-        // Esperas - Explícitas FluentWait
-        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-        driver.findElement(By.cssSelector("#checkbox-example > button")).click();
-        Wait<WebDriver> fluentWait = new FluentWait<>(driver)
-                .withTimeout(Duration.of(60, ChronoUnit.SECONDS))
-                .pollingEvery(Duration.of(2, ChronoUnit.SECONDS))
-                .ignoring(Exception.class);
-        WebElement fluentElement = fluentWait.until(webDriver -> webDriver.findElement(By.id("message")));
-        String fluentElementText = fluentElement.getText();
-        Assertions.assertThat(fluentElement.isDisplayed()).isTrue();
-        LOGGER.debug("finish element, fluentElementText:[{}]", fluentElementText);
-
-        driver.close();
-        LOGGER.debug("driver closed");
     }
 
 }
